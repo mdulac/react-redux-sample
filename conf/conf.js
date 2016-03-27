71c70a3f363edce12dc8f115ebb9aa951daf1fd9
mdulac <mdulac@xebia.fr>
2016-03-27 03:04:05 +0200

    Change port in dev (for Clever Cloud)

--- conf/conf.js
+++ conf/conf.js
@@ -11,7 +11,7 @@ module.exports = _.merge(
         middleware: {
             protocol: "http",
             host: "localhost",
-            port: 3000,
+            port: 8080,
             context: "/api/"
         }
     },
